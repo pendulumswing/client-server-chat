6eb4babf0b3c07ea3dc4797e2dde4e9f24b83ec8
pendulumswing <jason@pendulumswingmedia.com>
2022-05-27 10:19:08 -0700

    Single message exchange setup

--- code/client.py
+++ code/client.py
@@ -1,3 +1,28 @@
 from socket import *
 import sys
 import random
+
+serverName = "127.0.0.1"
+
+# Create random number within valid port range
+serverPort = 3580
+
+# USER PORT if provided at command line
+if len(sys.argv) > 1:
+    serverPort = int(sys.argv[1])
+
+# CREATE socket and connect to server
+clientSocket = socket(AF_INET, SOCK_STREAM)  # TCP connection
+clientSocket.connect((serverName, serverPort))
+
+request = input("Enter message to send...\n>")
+
+# SEND request to server
+clientSocket.send(request.encode())
+
+# GET response from server and print
+response = clientSocket.recv(1024)
+print("From Server: ", response.decode())
+
+# # CLOSE connection
+# clientSocket.close()
--- code/server.py
+++ code/server.py
@@ -1,3 +1,54 @@
 from socket import *
 import sys
 import random
+
+# Seed RNG
+random.seed()
+
+# PARAMETERS
+serverName = "127.0.0.1"
+
+# Create random number within valid port range
+# serverPort = random.randrange(1024, 49151, 1)
+serverPort = 3580
+response = "hello to you sir"
+
+# USER PORT if provided at command line
+if len(sys.argv) > 1:
+    serverPort = int(sys.argv[1])
+
+# CREATE listening socket and BIND to serverPort - TCP connection
+# 'with' statement for socket connection:
+# SOURCE: https://realpython.com/python-sockets/#tcp-sockets
+with socket(AF_INET, SOCK_STREAM) as serverSocket:
+    # ALLOW port to be reused immediately
+    # SOURCE: https://bit.ly/3NXGZeQ
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+    # BIND socket and LISTEN
+    serverSocket.bind(('', serverPort))
+    serverSocket.listen(1)
+    print(f'The server is listening on: {serverName} on port: {serverPort}')
+
+    # CREATE connection socket if requested by client
+    connectionSocket, addr = serverSocket.accept()
+    with connectionSocket:
+        print("Waiting for message...")
+        message = ""
+        while True:
+            # GET request from client and PRINT
+            message = connectionSocket.recv(1024).decode()
+            if not message:
+                break
+            print(message)
+
+            response = input("Enter message to send...\n>")
+
+            # SEND response to client and PRINT
+            connectionSocket.sendall(response.encode())
+
+            print("message sent")
+
+            # CLOSE connection and loop
+            connectionSocket.close()
+            break
